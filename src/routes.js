1664e5391103a877fc11adfa8bbcddca085445b3
Adyaksaimam <adyaksaimam@gmail.com>
2022-08-20 23:32:34 +0700

    2

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-const Course = React.lazy(() => import('./views/theme/colors/Course'))
-const Job = React.lazy(() => import('./views/theme/typography/Job'))
+const Dashboard = React.lazy(() => import("./views/dashboard/Dashboard"));
+const Course = React.lazy(() => import("./views/theme/course/Course"));
+const Job = React.lazy(() => import("./views/theme/job/Job"));
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
-  { path: '/Course', name: 'Course', element: Course },
-  { path: '/Job', name: 'Job', element: Job },
-]
+  { path: "/", exact: true, name: "Home" },
+  { path: "/dashboard", name: "Dashboard", element: Dashboard },
+  { path: "/Course", name: "Course", element: Course },
+  { path: "/Job", name: "Job", element: Job },
+];
 
-export default routes
+export default routes;
